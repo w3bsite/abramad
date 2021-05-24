928d1d81c0afe7086c1e0f92ede27db8ab1985ff
w3bsite <61429140+w3bsite@users.noreply.github.com>
2021-05-24 12:25:43 +0430

    first commit

---
+++ src/components/images.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    Images Comp
+    <div class="container bg-success rounded">
+      <div class="row d-flex">
+        <div class="col" v-for="(img, i) in obj" :key="i">
+          <img
+            style="max-height: 200px; max-width: 300px"
+            :src="'/images/' + i + '.jpg'"
+            alt=""
+            class="img-fluid rounded mx-auto d-block"
+          />
+          <button class="btn btn-danger btn-rounded" style="position: absolut">
+            save
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import json from "../assets/data.json";
+export default {
+  data() {
+    return {
+      obj: {},
+      src: "",
+    };
+  },
+
+  filters: {
+    filterimage(e) {
+      return (e = e.replace(/[^0-9]+/g, ""));
+    },
+  },
+
+  mounted() {
+    this.obj = json;
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/img.vue
@@ -0,0 +1,13 @@
+<template>
+  <Images class="container"></Images>
+</template>
+
+<script>
+import Images from "../components/images";
+export default {
+  components: { Images },
+};
+</script>
+
+<style>
+</style>
